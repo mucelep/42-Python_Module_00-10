9b735723cdb1b08ebd39161701d2b44a3e997e4f
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-08-05 20:10:19 +0000

    ex1_done

---
+++ Python_Module-07/capacitor.py
@@ -0,0 +1,49 @@
+from ex1 import HealingCreatureFactory, TransformCreatureFactory
+
+
+def healcreaturesfac(factory: HealingCreatureFactory) -> None:
+    base_creature = factory.create_base()
+    evolved_creature =  factory.create_evolved()
+
+    print("Testing Creature with healing capability")
+    print("Base:")
+
+    print(base_creature.describe())
+    print(base_creature.attack())
+    print(base_creature.heal())
+
+    print("Evolved:")
+    print(evolved_creature.describe())
+    print(evolved_creature.attack())
+    print(evolved_creature.heal())
+    print()
+
+
+def transcreaturesfac(factory: TransformCreatureFactory) -> None:
+    base_creature = factory.create_base()
+    evolved_creature =  factory.create_evolved()
+
+    print("Testing Creature with transform capability")
+    print("Base:")
+    print(base_creature.describe())
+    print(base_creature.attack())
+    print(base_creature.transform)
+    print(base_creature.attack())
+    print(base_creature.revert())
+
+    print("Evolved:")
+    print(evolved_creature.describe())
+    print(evolved_creature.attack())
+    print(evolved_creature.transform)
+    print(evolved_creature.attack())
+    print(evolved_creature.revert())
+
+def main() -> None:
+    healfac = HealingCreatureFactory()
+    transfac = TransformCreatureFactory()
+    healcreaturesfac(healfac)
+    transcreaturesfac(transfac)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python_Module-07/ex1/creatures.py
@@ -0,0 +1,64 @@
+from ex0.creatures import Creatures
+from .capabilities import HealCapability as heal, TransformCapability
+
+
+class Sproutling(Creatures, heal):
+    def __init__(self) -> None:
+        super().__init__("Sproutling", "Grass")
+
+    def attack(self) -> str:
+        return f"{self.name} uses Vine Whip!"
+
+    def heal(self) -> str:
+        return f"{self.name} heals itself for a small amount"
+
+
+class Bloomelle(Creatures, heal):
+    def __init__(self) -> None:
+        super().__init__("Bloomelle", "Grass/Fairy")
+
+    def attack(self) -> str:
+        return f"{self.name} uses Petal Dance!"
+
+    def heal(self) -> str:
+        return f"{self.name} heals itself and others for a large amount"
+
+
+class Shiftling(Creatures, TransformCapability):
+    def __init__(self) -> None:
+        Creatures.__init__(self, "Shiftling", "Normal")
+        TransformCapability.__init__(self)
+
+    def attack(self) -> str:
+        if not self.transformed:
+            return f"{self.name} attacks normally."
+        else:
+            return f"{self.name} performs a boosted strike!"
+
+    def transform(self) -> str:
+        self.transformed = True
+        return f"{self.name} shifts into a sharper form!"
+
+    def revert(self) -> str:
+        self.transformed = False
+        return f"{self.name} returns to normal."
+
+
+class Morphagon(Creatures, TransformCapability):
+    def __init__(self) -> None:
+        Creatures.__init__(self, "Morphagon", "Normal/Dragon")
+        TransformCapability.__init__(self)
+
+    def attack(self) -> str:
+        if not self.transformed:
+            return f"{self.name} attacks normally."
+        else:
+            return f"{self.name} unleashes a devastating morph strike!"
+
+    def transform(self) -> str:
+        self.transformed = True
+        return f"{self.name} morphs into a dragonic battle form!"
+
+    def revert(self) -> str:
+        self.transformed = False
+        return f"{self.name} stabilizes its form."
