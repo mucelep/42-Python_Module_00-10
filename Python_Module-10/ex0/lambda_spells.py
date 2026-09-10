47e755a9eef4c5c1398f35246ca0e0b045ade24b
Muhammed Omer Celep <mucelep.celepm82@gmail.com>
2026-09-10 21:18:14 +0300

    ex0 flake8 and mypy --strict

--- Python_Module-10/ex0/lambda_spells.py
+++ Python_Module-10/ex0/lambda_spells.py
@@ -1,12 +1,15 @@
+from typing import Any
 
-def artifact_sorter(artifacts: list[dict]) -> list[dict]:
-    result: list[dict] = sorted(artifacts, key=lambda x: x["power"], reverse=True)
+
+def artifact_sorter(artifacts: list[dict[str, Any]]) -> list[dict[str, Any]]:
+    result: list[dict[str, Any]] = sorted(
+        artifacts,
+        key=lambda x: x["power"], reverse=True)
     return result
 
 
-
-
-def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+def power_filter(mages: list[dict[str, Any]], min_power: int
+                 ) -> list[dict[str, Any]]:
     result = filter(lambda mage: mage["power"] > min_power, mages)
     return list(result)
 
@@ -15,15 +18,17 @@ def spell_transformer(spells: list[str]) -> list[str]:
     result = map(lambda spell: "* "+spell+" *", spells)
     return list(result)
 
-def mage_stats(mages: list[dict]) -> dict:
+
+def mage_stats(mages: list[dict[str, Any]]) -> dict[str, Any]:
     hpower = max(mages, key=lambda m: m["power"])
     lpower = min(m["power"] for m in mages)
     average = round(sum(m["power"] for m in mages) / len(mages), 2)
-    return{
+    return {
         "max_power": int(hpower["power"]),
         "min_power": int(lpower["power"]),
         "avg_power": float(average)
     }
+
 
 def main() -> None:
     print("Testing artifact sorter...")
@@ -32,19 +37,22 @@ def main() -> None:
         "power": 92,
         "tpye": "fire"
     }
+
     orb = {
         "name": "Crystal Orb",
         "power": 85,
         "tpye": "Crystal"
     }
+
     x = artifact_sorter([fire, orb])
-    print(f"{x[0]["name"]}({x[0]["power"]} power) comes"
-          f"before {x[1]["name"]} ({x[1]["power"]} power)\n")
+    print(f'{x[0]["name"]}({x[0]["power"]} power) comes'
+          f'before {x[1]["name"]} ({x[1]["power"]} power)\n')
 
     print("Testing spell transformer...")
     trans = spell_transformer(["fireball", "heal", "shield"])
     for i in range(3):
-        print(f"{trans[i]} ",end="")
+        print(f"{trans[i]} ", end="")
+
 
 if __name__ == "__main__":
     main()
