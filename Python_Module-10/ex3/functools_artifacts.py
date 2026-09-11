9dfcab12148c702eec411aeb5b168944a99bbf5f
Muhammed Ömer CELEP <celepm82@gmail.com>
2026-09-11 15:37:24 +0000

    ex3 start

---
+++ Python_Module-10/ex3/functools_artifacts.py
@@ -0,0 +1,22 @@
+import functools
+import operator
+from collections.abc import Callable
+from typing import Any
+
+
+def spell_reducer(spells: list[int], operation: str) -> int:
+    pass
+def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
+    pass
+def memoized_fibonacci(n: int) -> int:
+    pass
+def spell_dispatcher() -> Callable[[Any], str]:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
